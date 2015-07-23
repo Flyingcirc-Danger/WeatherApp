d4940b783bf6946a5482433fa5ae9223492d4590
Tom Bryant <tomcbryant@gmail.com>
2015-07-23 12:08:29 +0100

    basic front-end (input bar)

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tom_Bryant
+ * Date: 7/23/15
+ * Time: 11:17 AM
+ */
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Project Juicebox</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../dist/css/weather.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="grid.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+
+
+
+
+
+<body style="background-color:#2196F3;">
+<div class="page">
+    <div class ="middle">
+<div class ="cityInput">
+    <center><table>
+        <tr>
+            <form>
+                <td class="inputDesc" >City or Zip Code:  </td>
+                <td><input type="text" class="form-control inputBar"></td>
+                <td><button type="button" class="btn btn-primary">Go</button></td>
+            </form>
+        </tr>
+    </table>
+    </center>
+</div>
+    </div>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
